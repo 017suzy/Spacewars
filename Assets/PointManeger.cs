45310b609638071f021137c3ca5830544cd19114
Man0ela <163747420+Man0ela@users.noreply.github.com>
2024-12-18 18:26:51 -0300

    pontuaçao
    implementei a pontuação da nave, cada hit é 50 de dano

---
+++ Assets/PointManeger.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using TMPro;
+public class PointManeger : MonoBehaviour
+{   public int score;
+    public TMP_Text scoreText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void UpdateScore(int points){
+        score += points;
+        scoreText.text= "Pontuaçao: " + score;
+    }
+}
--- Assets/script/atirar.cs
+++ Assets/script/atirar.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class atirar : MonoBehaviour
 {
     public float moveSpeed;
+    private PointManeger pointManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        pointManager = GameObject.Find("PointManeger").GetComponent<PointManeger>();
     }
 
     // Update is called once per frame
@@ -19,6 +20,7 @@ public class atirar : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision){
         if (collision.gameObject.tag == "Enemy"){
             Destroy(collision.gameObject);
+            pointManager.UpdateScore(50);
             Destroy(gameObject);
         }
         if (collision.gameObject.tag == "barreira"){
