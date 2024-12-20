38088908e70effc29a82114e6c5f3aff57d8a777
suzy <ryenh017@gmail.com>
2024-12-20 20:41:04 -0300

    creditos

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,6 +6,13 @@ public class MainMenu : MonoBehaviour
     public void PlayGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
+    public void Credits(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+2);
+    }
+
+    public void BackToMenu(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-2);
+    }
 
     public void QuitGame(){
         Application.Quit();
