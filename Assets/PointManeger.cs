de1fa87e1d4c3f498a7d9fabe22a83e70d7a18c1
suzy <ryenh017@gmail.com>
2024-12-21 14:55:45 -0300

    highscore

--- Assets/PointManeger.cs
+++ Assets/PointManeger.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
 using TMPro;
 public class PointManeger : MonoBehaviour
-{   public int score;
+{   
+    public int score = 0;
+    public int highscore = 0;
+
+
     public TMP_Text scoreText;
+    public TMP_Text highscoreText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //only in my computer
+        highscore = PlayerPrefs.GetInt("highscore", 0);
+        scoreText.text = "SCORE:" + score.ToString();
+        highscoreText.text = "HIGHSCORE:" + highscore.ToString();
     }
 
     public void UpdateScore(int points){
         score += points;
         scoreText.text= "SCORE: " + score;
+        if (highscore<score)
+        {
+            PlayerPrefs.SetInt("highscore", score);
+        }
     }
 }
